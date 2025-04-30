1e97b66e3f1580586ebbc663e7d16cc5e1681a8f
Apptrader <proyectoapptrader@gmail.com>
2025-04-30 15:35:48 -0300

    merge

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,18 +1,11 @@
-// routes/paymentRoutes.js
+
 import express from 'express';
-import {
-  paymentIntent,      // Stripe
-  initMercadoPago,    // ← NUEVO: crea orden + preferencia
-  mpWebhook           // ← NUEVO: recibe notificaciones
-} from '../controllers/paymentController.js';
+import { create_preference, paymentIntent } from '../controllers/paymentController.js';
 
 const router = express.Router();
 
-/*────────── STRIPE ──────────*/
 router.post('/intent', paymentIntent);
+router.post('/mp/create-preference', create_preference)
 
-/*──────── MERCADO PAGO ───────*/
-router.post('/mp/init', initMercadoPago);          // 1) orden + preferencia
-router.post('/webhooks/mercadopago', mpWebhook);   // 2) confirmación (sin token)
 
 export default router;
